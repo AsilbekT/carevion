28864d2ea1ea880a29a7783e968bd5a3cdab42f9
Ismoiljon007 <ibahtiyorov007@gmail.com>
2025-04-19 15:32:46 +0500

    add full project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -452,7 +452,7 @@
                 src="https://maps.google.com/maps?q=New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed"
                 frameborder="0"
                 style="border: 0; width: 100%; height: 350px"
-                allowfullscreen=""
+                allowfullscreen
               ></iframe>
             </div>
           </div>
@@ -462,7 +462,7 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { onMounted } from "vue";
 import { useSlickSliders } from "@/composables/useSlickSliders";
 
